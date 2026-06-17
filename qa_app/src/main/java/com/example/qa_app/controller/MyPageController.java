35548f4dda8043f44ebfc82ab80ccabf99298af1
Watanabe <=2026044@fsc-educ.com>
2026-06-17 19:00:44 +0900

    0617 19:00時点

--- qa_app/src/main/java/com/example/qa_app/controller/MyPageController.java
+++ qa_app/src/main/java/com/example/qa_app/controller/MyPageController.java
@@ -1,5 +1,13 @@
 package com.example.qa_app.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MyPageController {
+    @GetMapping("/mypage")
+    public String GetMethodName() {
+        return "mypage";
+    }
     
 }
