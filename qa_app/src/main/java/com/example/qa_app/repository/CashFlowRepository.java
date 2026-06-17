9e5e22b64888e51992d6f3d881f90105e4b64718
Furugo <2026003@fsc-educ.com>
2026-06-17 17:07:56 +0900

    画面ドラフト、サービス、リポジトリ②

--- qa_app/src/main/java/com/example/qa_app/repository/CashFlowRepository.java
+++ qa_app/src/main/java/com/example/qa_app/repository/CashFlowRepository.java
@@ -1,5 +1,57 @@
 package com.example.qa_app.repository;
 
+import java.util.List;
+
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+
+import com.example.qa_app.model.DTO.Daily;
+
+@Repository
 public class CashFlowRepository {
-    
+    private final JdbcClient jdbcClient;
+
+    public CashFlowRepository(JdbcClient jdbcClient){
+        this.jdbcClient = jdbcClient;
+    }
+
+
+    // 今月分の支出のみのデータを取得
+    public List<Daily> findExpensesThisMonth(int user_id){
+        return jdbcClient.sql("SELECT date, category_name, in_out, daily_amount FROM daily WHERE user_id = :user_id AND in_out = TRUE AND date BETWEEN DATE_FORMAT(NOW(), '%Y-%m-01') AND CURRENT_DATE ORDER BY date DESC, daily_amount DESC;")
+                .query(Daily.class)
+                .list();
+    }
+
+
+    // 今月分の収支データを取得
+    public List<Daily> findAllThisMonth(int user_id){
+        return jdbcClient.sql("SELECT date, category_name, in_out, daily_amount FROM daily WHERE user_id = :user_id AND date BETWEEN DATE_FORMAT(NOW(), '%Y-%m-01') AND CURRENT_DATE ORDER BY date DESC, daily_amount DESC;")
+        .query(Daily.class)
+        .list();
+    }
+
+
+    // dailyテーブルから特定のレコードを削除
+    public void deleteCashFlowItem(Long id){
+        jdbcClient.sql("DELETE FROM daily WHERE id = :id")
+        .param("id", id)
+        .update();
+    }
+
+
+    // 先月の支出のみのデータを取得
+    public List<Daily> findExpensesLastMonth(int user_id){
+        return jdbcClient.sql("SELECT date, category_name, in_out, daily_amount FROM daily WHERE user_id = :user_id AND in_out = TRUE AND date BETWEEN DATE_FORMAT(NOW()-INTERVAL 1 MONTH, '%Y-%m-01') AND LAST_DAY(NOW() - INTERVAL 1 MONTH) ORDER BY date DESC, daily_amount DESC; ")
+        .query(Daily.class)
+        .list();
+    }
+
+
+    // 先月の収支データを取得
+    public List<Daily> findAllLastMonth(int user_id){
+        return jdbcClient.sql("SELECT date, category_name, in_out, daily_amount FROM daily WHERE user_id = :user_id AND date BETWEEN DATE_FORMAT(NOW()-INTERVAL 1 MONTH, '%Y-%m-01') AND LAST_DAY(NOW() - INTERVAL 1 MONTH) ORDER BY date DESC, daily_amount DESC;")
+        .query(Daily.class)
+        .list();
+    }
 }
--- qa_app/src/main/java/com/example/qa_app/service/CashFlowService.java
+++ qa_app/src/main/java/com/example/qa_app/service/CashFlowService.java
@@ -1,5 +1,54 @@
 package com.example.qa_app.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.qa_app.model.DTO.Daily;
+import com.example.qa_app.repository.CashFlowRepository;
+
+@Service
 public class CashFlowService {
-    
+    private final CashFlowRepository cashFlowRepository;
+
+    public CashFlowService(CashFlowRepository cashFlowRepository) {
+        this.cashFlowRepository = cashFlowRepository;
+    }
+
+    // 今月分の支出の合計金額を計算
+    public int calcExpensesThisMonth(int user_id) {
+        List<Daily> expenses = cashFlowRepository.findExpensesThisMonth(user_id);
+        int sum = 0;
+        for (int i = 0; i < expenses.size(); i++) {
+            sum += expenses.get(i).getDailyAmount();
+        }
+        return sum;
+    };
+
+
+    // 今月分の収支のデータを表示
+    public List<Daily> showAllThisMonth(int user_id) {
+        return cashFlowRepository.findAllThisMonth(user_id);
+    }
+
+    // 今月分の収支のデータを削除
+    public void deleteCashFlowItem(Long id) {
+        cashFlowRepository.deleteCashFlowItem(id);
+    }
+
+    // 先月の支出の合計金額を計算
+    public int calcExpensesThisMonth(int user_id) {
+        List<Daily> expenses = cashFlowRepository.findExpensesLastMonth(user_id);
+        int sum = 0;
+        for (int i = 0; i < expenses.size(); i++) {
+            sum += expenses.get(i).getDailyAmount();
+        }
+        return sum;
+    }
+
+    // 先月の収支のデータを表示
+    public List<Daily> showAllLastMonth(int user_id) {
+        return cashFlowRepository.findAllLastMonth(user_id);
+    }
 }
