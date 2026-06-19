77512fa432a624f903a3ca247554f1d6bd96e571
Yanagiuchi <2026059@fsc-educ.com>
2026-06-19 11:39:52 +0900

    RegisterFromに新規登録処理を追加

--- qa_app/src/main/java/com/example/qa_app/model/Entity/RegisterFrom.java
+++ qa_app/src/main/java/com/example/qa_app/model/Entity/RegisterFrom.java
@@ -7,6 +7,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class RegisterFrom {
+    // 新規登録時に使用
+    private String email;
+    private String password;
+    // 初期パラメータ設定に使用
+    // ユーザーが入力した収入、固定費、カテゴリ、金額を取得
     private Integer incomeAmount;
     private List<String> categories;
     private List<String> fixedNames;
